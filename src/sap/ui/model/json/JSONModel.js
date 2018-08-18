dd709f92568a68520dbdf8ba0f06afcf615814ad
ysokol <sokolovskij-yurij@yandex.ru>
2018-08-18 23:25:38 +0300

    1

--- src/sap/ui/model/json/JSONModel.js
+++ src/sap/ui/model/json/JSONModel.js
@@ -1,11 +1,13 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/json/JSONPropertyBinding"
-], function(JSONModel, JSONPropertyBinding) {
+	"sap/ui/model/json/JSONPropertyBinding",
+	"sap/ui/model/Binding"
+], function(JSONModel, JSONPropertyBinding, Binding) {
 	"use strict";
 	return JSONModel.extend("com.pepsico.core.sap.ui.model.json.JSONModel", {
 		attachChange: function(sPath, fnHandler) {
-			let oBinding = new JSONPropertyBinding(this, sPath, this.getContext(sPath));
+			//let oBinding = new JSONPropertyBinding(this, sPath, this.getContext(sPath));
+			let oBinding = new Binding(this, sPath, this.getContext(sPath));
 			oBinding.attachChange(fnHandler, this);
 		}
 	});
